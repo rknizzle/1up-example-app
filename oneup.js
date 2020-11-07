055e650d57516bf8cb97d52a102689ea33a91061
rknizzle <ryan@autotiv.com>
2020-11-07 14:05:32 -0500

    add a fxn to get an auth code for a 1up user

--- oneup.js
+++ oneup.js
@@ -21,6 +21,28 @@ function createUser(username) {
   })
 }
 
+// get an auth code for a 1up user that can be exchanged for an access token
+function getAuthCodeForExistingUser(userId) {
+  return axios({
+    method: 'POST',
+    validateStatus: () => true,
+    url: 'https://api.1up.health/user-management/v1/user/auth-code',
+    data: {
+      app_user_id: userId,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+    }
+  })
+  .then((res) => {
+    if (res.data.success) {
+      return res.data
+    } else {
+      throw new Error(`getAuthCodeForExistingUser failed with status code ${res.status} and error: '${res.data.error}'`)
+    }
+  })
+}
+
 module.exports = {
   createUser,
+  getAuthCodeForExistingUser,
 }
