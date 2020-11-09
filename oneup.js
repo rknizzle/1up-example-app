11dfafbb2278bfa6fdb320e876cba1263a2319a0
rknizzle <ryan@autotiv.com>
2020-11-08 21:21:42 -0500

    add fxn to get all of a patients fhir data

--- oneup.js
+++ oneup.js
@@ -105,9 +105,58 @@ function getPatientId(accessToken) {
   })
 }
 
+// get a single page of fhir data for a patient
+function getFhirDataPage(accessToken, fhirVersion, patientId, nextUrl) {
+  if (nextUrl) {
+    url = nextUrl
+  } else {
+    url = `https://api.1up.health/fhir/${fhirVersion}/Patient/${patientId}/$everything`
+  }
+
+  return axios({
+    method: 'GET',
+    validateStatus: () => true,
+    url: url,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    }
+  })
+  .then((res) => {
+    if (res.status === 200) {
+      return res.data
+    } else {
+      throw new Error(`getFhirPageData failed with status code ${res.status} and error ${res.data.error}`)
+    }
+  })
+}
+
+async function getAllFhirData(accessToken, fhirVersion, patientId) {
+  let allData = []
+  let res
+  let nextUrl = undefined
+
+  do {
+    // get a page of FHIR data
+    res = await getFhirDataPage(accessToken, fhirVersion, patientId, nextUrl)
+
+    // add the data to the full list of patient data
+    allData = [].concat(allData, res.entry)
+
+    // if theres a link in the response, there are no more resources to fetch
+    moreResourcesRemaining = res.link
+
+    // if there are more resources to fetch, get the next url
+    if (moreResourcesRemaining) nextUrl = res.link[1].url
+
+  } while (moreResourcesRemaining)
+
+  return allData
+}
+
 module.exports = {
   createUser,
   getAccessToken,
+  getAllFhirData,
   getAuthCodeForExistingUser,
   getPatientId,
   refreshAccessToken,
